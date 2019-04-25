9407565506d774fb9ac9427ec85adc78aef1297b
seanjasm <seanjasm@gmail.com>
2019-04-24 22:51:28 -0400

    lab 6 first commit

---
+++ DiceRollSimulator/DiceRollSimulator/DiceRollerApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollSimulator
+{
+    class DiceRollerApp
+    {
+        public static int GetSidesNumberFromUser(string message)
+        {
+            Console.WriteLine(message);
+
+            if (!int.TryParse(Console.ReadLine(), out int userNumber))
+            {
+                userNumber = GetSidesNumberFromUser("Input not valid! Try again.  ");
+            }
+
+            return userNumber;
+        }
+
+        public static int GetRoll(int max, Random random)
+        {
+            return random.Next(1, max);
+        }
+
+
+        public static bool Confirmation(string message, string trueOption, string falseOption)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine().ToLower();
+
+            if (input == trueOption.ToLower())
+            {
+                return true;
+            }
+            else if(input == falseOption)
+            {
+                return false;
+            }
+
+            return Confirmation("Input is not valid." + message, trueOption, falseOption);
+        }
+
+        public static string SpecialMessage(int[] dice, int max)
+        {
+            //Return this if dice roll landed on highest number on each die
+            if (dice[0] == max && dice[1] == max)
+            {
+                return "Box cars!".ToUpper();
+            }
+            else if (dice[0] == 1 && dice[1] == 1)
+            {
+                return "Snake eyes!".ToUpper();
+            }
+            else
+            {
+                return "Craps!";
+            }
+        }
+    }
+}
---
+++ DiceRollSimulator/DiceRollSimulator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dice = new int[] { 0, 0 };
+            Random random = new Random();
+            int counter = 1;
+            int sides;
+
+            Console.WriteLine("\n\n============================================\n" +
+                "\tWelcome to Grand Circus Casino!\n" +
+                "============================================\n\n\n");
+                sides = DiceRollerApp.GetSidesNumberFromUser("How many sides ?");
+
+            while (DiceRollerApp.Confirmation("Roll?<Y/N>  ", "y", "n"))
+            {
+                dice[0] = DiceRollerApp.GetRoll(sides, random);
+                dice[1] = DiceRollerApp.GetRoll(sides, random);
+
+                Console.WriteLine($"\n\nRoll {counter}: {DiceRollerApp.SpecialMessage(dice, sides)}" +
+                    $"\n\nDie1:{dice[0]}   Die2:{dice[1]}\n\n");
+                counter++;
+            } 
+        }        
+    }
+}
